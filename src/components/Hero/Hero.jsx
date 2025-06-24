2c7be7147f93f763260e77d2a0eb58a894cbc7e9
Muthukumaran1365 <muth22097.cs@rmkec.ac.in>
2025-06-24 08:43:33 +0530

    Update Hero section and styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -7,9 +6,13 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Muthu</h1>
-        <p className={styles.description}>Currently pursuing a B.E in Computer Science and Engineering at R.M.K Engineering College</p>
-        <p className={styles.description}>Aspiring Full-Stack Software Engineer | Passionate About Technology, Innovation, and Problem Solving</p>
+        <h1 className={styles.title}>Hi, I'm Muthu Kumaran</h1>
+        <p className={styles.description}>
+          Currently pursuing a B.E in Computer Science and Engineering at R.M.K Engineering College
+        </p>
+        <p className={styles.description}>
+          Aspiring Full-Stack Software Engineer | Passionate About Technology, Innovation, and Problem Solving
+        </p>
         <a href="mailto:muthurock1352@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
